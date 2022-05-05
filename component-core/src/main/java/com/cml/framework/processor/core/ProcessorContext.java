f3991c120bbc43c7ea8dc2115f55007f950f9c4b
admin <865517964@qq.com>
2022-05-05 21:34:13 +0800

    d

--- component-core/src/main/java/com/cml/framework/processor/core/ProcessorContext.java
+++ component-core/src/main/java/com/cml/framework/processor/core/ProcessorContext.java
@@ -1,5 +1,7 @@
 package com.cml.framework.processor.core;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 流程上下文
  *
@@ -8,4 +10,13 @@ package com.cml.framework.processor.core;
  * @createTime 2021年10月17日 20:53:00
  */
 public class ProcessorContext {
+    private ConcurrentHashMap<String, Object> extras = new ConcurrentHashMap<>();
+
+    public void putExtra(String k, Object v) {
+        extras.put(k, v);
+    }
+
+    public <T> T take(String key) {
+        return (T) extras.get(key);
+    }
 }
